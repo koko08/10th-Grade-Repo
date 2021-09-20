c38e48143823cb84fee275471bb5295fd3203860
koko08 <78847028+koko08@users.noreply.github.com>
2021-09-20 08:38:47 +0300

    Add files via upload

---
+++ 20.09.2021 Десетично към Двоично.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _20._09._2021_Упр
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int desetichno = int.Parse(Console.ReadLine());
+            string dvoichno = Convert.ToString(desetichno, 2);
+            Console.WriteLine("dvoichno: " + dvoichno);
+        }
+    }
+}
