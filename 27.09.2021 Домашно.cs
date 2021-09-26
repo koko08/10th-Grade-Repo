54743a6a36ebfbede65ae85b2a2c79094316bd90
koko08 <78847028+koko08@users.noreply.github.com>
2021-09-26 20:56:53 +0300

    Add files via upload

---
+++ 27.09.2021 Домашно.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2: ");
+            string dv = Console.ReadLine();
+            int dvc = Convert.ToInt32(dv, 2);
+            Console.WriteLine(dvc);
+            string sh = Console.ReadLine();
+            int shc = Convert.ToInt32(sh, 16);
+            Console.WriteLine(shc);
+            int ans = dvc + shc;
+            Console.WriteLine(ans);
+        }
+    }
+}
